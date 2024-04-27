611f852080fe269dfbebc72ef6ce0c35b74676c0
vagner ventura <vagnerventura@hotmail.com>
2024-04-27 20:19:09 -0300

    User Entity

---
+++ src/main/java/com/nelioAlves/workshopmongo/resources/UserResource.java
@@ -0,0 +1,29 @@
+package com.nelioAlves.workshopmongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioAlves.workshopmongo.domain.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity <List<User>> findAll(){
+		User Maria = new User("1","Maria Brow", "maria@gmail.com"); 
+		User Alex = new User("2","Alex Green", "alex@gmail.com");
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(Maria,Alex));
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
+	
+}
