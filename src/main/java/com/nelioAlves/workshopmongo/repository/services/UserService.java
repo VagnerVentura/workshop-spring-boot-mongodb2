e093c2f98ec7af8e3147666a886989ddf282f15f
vagner ventura <vagnerventura@hotmail.com>
2024-04-29 19:42:47 -0300

    Obtendo um usuário por id

--- src/main/java/com/nelioAlves/workshopmongo/repository/services/UserService.java
+++ src/main/java/com/nelioAlves/workshopmongo/repository/services/UserService.java
@@ -1,12 +1,14 @@
 package com.nelioAlves.workshopmongo.repository.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioAlves.workshopmongo.domain.User;
 import com.nelioAlves.workshopmongo.repository.UserRepository;
+import com.nelioAlves.workshopmongo.repository.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,5 +21,12 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+	Optional<User> obj = repo.findById(id);
+	return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
 	
 }
