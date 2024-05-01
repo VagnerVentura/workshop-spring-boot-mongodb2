d720df46cc1645d6e1c6492d3aaeea3a467216e2
vagner ventura <vagnerventura@hotmail.com>
2024-04-30 22:30:21 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/nelioAlves/workshopmongo/repository/services/UserService.java
+++ src/main/java/com/nelioAlves/workshopmongo/repository/services/UserService.java
@@ -14,31 +14,41 @@ import com.nelioAlves.workshopmongo.repository.services.exception.ObjectNotFound
 @Service
 public class UserService {
 
-	
 	@Autowired
 	private UserRepository repo;
-	
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
-	Optional<User> obj = repo.findById(id);
-	return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
-	public User insert(User obj ) {
+
+	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
-	public User fromDTO(UserDTO objDto) {
-		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
-	}
-	
-	public void delete(String id){
+
+	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
+	}
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
--- src/main/java/com/nelioAlves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioAlves/workshopmongo/resources/UserResource.java
@@ -6,13 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelioAlves.workshopmongo.domain.User;
@@ -53,6 +47,13 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PutMapping(value="/{id}")
+ 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 	
 	
 	
